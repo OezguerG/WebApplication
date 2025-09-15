d554cd3e2b969323d0e2fe19b25d0c960128a1d7
oegu9453 <oegu9453@bht-berlin.de>
2025-09-15 18:55:51 +0200

    changes for publication 3

--- server/src/model/GebietModel.ts
+++ server/src/model/GebietModel.ts
@@ -1,5 +1,5 @@
 import { Schema, model, Types } from "mongoose"
-import { Prof } from "../../src/model/ProfModel";
+import { Prof } from "./ProfModel";
 
 export interface IGebiet {
     name: string
