c48c5260980b55d64732d6535676ce7ad8f81334
OezguerG <guleryuzozgur123@gmail.com>
2025-09-19 13:07:53 +0200

    fixed syntax error

--- client/src/components/PrefContext.Provider.tsx
+++ client/src/components/PrefContext.Provider.tsx
@@ -5,15 +5,15 @@ import { PrefContext, PrefContextState } from "./PrefContext";
 export const PrefContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => { 
 
   const setDefaultsGebiet = (id: String) => {
-    
+    id = "2";
   }
 
   const setDefaultsThemen = (id: string) => {
-    
+    id = "2";
   }
 
   const setProfChanges = (id: string) => {
-    
+    id = "2";
   }
 
   const contextValue: PrefContextState = {
