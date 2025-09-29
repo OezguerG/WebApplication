b815ddda38521ac96cbaec7fc368caaf0a6939d7
guleryuzozgur123@gmail.com <oegu9453@bht-berlin.de>
2025-09-29 17:59:28 +0200

    fixed login buggs continued

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,15 +1,20 @@
 import { Navigate } from "react-router-dom";
 import { useLoginContext } from "../LoginContext";
 import { ReactNode } from "react";
+import { LoadingIndicator } from "./LoadingIndicator";
 
 type PrivateRouteProps = {
     children: ReactNode;
 };
 
 export const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-    const { isLoggedIn } = useLoginContext();
+    const { login} = useLoginContext();
 
-    if (!isLoggedIn) {
+    if (login === undefined) {
+        return <LoadingIndicator />;
+    }
+
+    if (login === false) {
         return <Navigate to="/login" replace />;
     }
 
